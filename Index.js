926d8a3c7ca6c9fe8654d71d6751dc6c4306f21e
AmisterdanCLima <amisterdandeveloper@gmail.com>
2022-11-02 15:25:21 -0300

    "criando um erro"

--- Index.js
+++ Index.js
@@ -1,4 +1,5 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
 
@@ -6,12 +7,8 @@ import { ContaPoupanca } from "./ContaPoupanca.js";
 const cliente01 = new Cliente("Amisterdan", "00464761301",99010210937);
 
 const contaAmisterdan = new ContaCorrente(cliente01, 1001);
-contaAmisterdan.depositar(500);
-contaAmisterdan.sacar(100);
-
 const contaPoupanca = new ContaPoupanca(50,cliente01,1001);
-contaPoupanca.depositar(100);
-contaPoupanca.sacar(10);
 
-console.log(contaPoupanca);
-console.log(contaAmisterdan);
+
+
+
